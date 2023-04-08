6711a73cc77eba19fff6545ee8791f5535b50fec
22HOP2E_Munkhorgil <Munkhoirgil@Mstars-Lab1-08s-iMac.local>
2023-04-08 12:01:17 +0800

    header

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,11 +1,27 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 export const Header = () => {
   return (
     <div className="Header">
-      <div>
-        <img src="" alt="logo" />
+      <div className="HeaderOne">
+        <img
+          style={{ height: "60px", width: "60px" }}
+          src="https://scontent.fuln2-2.fna.fbcdn.net/v/t1.6435-9/164144912_227892525437339_881748644955352896_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=LSqyCE8Ni5MAX_rHeo2&_nc_ht=scontent.fuln2-2.fna&oh=00_AfDO4LlGEfr75GkmppKTLcS2BYkgwhtCMQSqlR6WT_JsAw&oe=645607FC "
+          alt="Logo"
+        />
+        <div>search</div>
+        <div className="headerSign">
+          <div style={{ marginRight: "10px" }}>sign up</div>
+          <div> sign in</div>
+        </div>
       </div>
-      <div>
-
+      <div className="HeaderTwo">
+        <div>Home</div>
+        <div>About</div>
+        <div>Food</div>
+        <div>Travel</div>
+        <div>Repair</div>
+        <div>Massage</div>
+        <div>Relax</div>
       </div>
     </div>
   );
