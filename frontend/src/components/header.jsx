f979a27b425609920b60f8cb9550d91fdcd691d4
22HOP2E_Munkhorgil <Munkhoirgil@Mstars-Lab1-08s-iMac.local>
2023-04-11 17:11:28 +0800

    Header

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,27 +1,33 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 export const Header = () => {
   return (
     <div className="Header">
-      <div className="HeaderOne">
-        <img
-          style={{ height: "60px", width: "60px" }}
-          src="https://scontent.fuln2-2.fna.fbcdn.net/v/t1.6435-9/164144912_227892525437339_881748644955352896_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=LSqyCE8Ni5MAX_rHeo2&_nc_ht=scontent.fuln2-2.fna&oh=00_AfDO4LlGEfr75GkmppKTLcS2BYkgwhtCMQSqlR6WT_JsAw&oe=645607FC "
-          alt="Logo"
-        />
-        <div>search</div>
-        <div className="headerSign">
-          <div style={{ marginRight: "10px" }}>sign up</div>
-          <div> sign in</div>
+      <div className="headerTop">
+        <div>Servicefy</div>
+        <div className="sign">
+          <div>Sign Up</div>
+          <div>Sign In</div>
         </div>
       </div>
-      <div className="HeaderTwo">
-        <div>Home</div>
-        <div>About</div>
-        <div>Food</div>
-        <div>Travel</div>
-        <div>Repair</div>
-        <div>Massage</div>
-        <div>Relax</div>
+      <div className="headerBottom">
+        <div className="headerImg">
+          <img
+            style={{ width: "60px", height: "53px" }}
+            src="https://scontent.fuln8-1.fna.fbcdn.net/v/t39.30808-6/340010607_560679526153438_7590188428266577079_n.jpg?stp=cp6_dst-jpg&_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_ohc=wX-pJTsogaMAX-GRfOu&_nc_ht=scontent.fuln8-1.fna&oh=00_AfDdU1zntlmbff3ehFzY4Dku989A2pg1cjUyv9ZZpLFdSw&oe=64365922"
+            alt="logo"
+          />
+        </div>
+        <div className="headerMain">
+          <div>Home</div>
+          <div>About</div>
+          <div>Food</div>
+          <div>Travel</div>
+          <div>Repair</div>
+          <div>Massage</div>
+          <div>Relax</div>
+        </div>
+        <div className="headerInput">
+          <input type="search" name="" id="" placeholder="Search" />
+        </div>
       </div>
     </div>
   );
