ff31ae290d356771fa63da59c42ec5023c4e871c
Barsaa75 <110914653+Barsaa75@users.noreply.github.com>
2023-05-09 15:47:33 +0800

    a

--- frontend/src/pages/Relax.jsx
+++ frontend/src/pages/Relax.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "../App.css";
 import { useEffect, useState } from "react";
 import { instance } from "../App";
-import { Link } from "react-router-dom";
 import JumpItems from "../components/JumpItems";
 import { Header } from "../components/header";
 function Relax() {
